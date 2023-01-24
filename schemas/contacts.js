5f24f7db0b1288b388865385baf673eca32f5ef3
Roman <romanuch06.88@gmail.com>
2023-01-24 10:58:40 +0200

    Update contacts.js

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
 
-const schemaContact = Joi.object({
+const schemaAddContact = Joi.object({
   name: Joi.string().min(3).required(),
   email: Joi.string()
     .email({
@@ -11,4 +11,13 @@ const schemaContact = Joi.object({
   phone: Joi.string().min(6).required(),
 });
 
-module.exports = schemaContact;
+const schemaUpdateContact = Joi.object({
+  name: Joi.string().min(3),
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ['com', 'net', 'org'] },
+  }),
+  phone: Joi.string().min(6),
+});
+
+module.exports = { schemaAddContact, schemaUpdateContact };
