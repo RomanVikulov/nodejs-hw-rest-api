fa927e012e73f80a9f7013249e987b79835e3743
Roman <vikulov.roman.work@gmail.com>
2023-02-06 10:52:46 +0200

    Update contactsController.js

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,34 +1,42 @@
-const createError = require("http-errors");
+const { NotFound } = require('http-errors');
 const service = require("../service/contactsService");
 
 // Get all contacts
 const getAll = async (req, res) => {
-  const result = await service.getAll();
+  const { _id } = req.user;
+  const result = await service.getAll(_id, req.query);
   res.json(result);
 };
 
 // Get contact by id
 const getById = async (req, res, next) => {
   const { contactId } = req.params;
-  const result = await service.getById(contactId);
+  const { _id } = req.user;
+  const result = await service.getById(contactId, _id);
   if (!result) {
-    return next(createError(404, `Not found contact with id: ${contactId}`));
+    return next(new NotFound(`Not found contact with id: ${contactId}`));
   }
   res.json(result);
 };
 
 // Add new contact
 const addContact = async (req, res) => {
-  const result = await service.addContact(req.body);
+  const { _id } = req.user;
+  const result = await service.addContact({ ...req.body, owner: _id });
   res.status(201).json(result);
 };
 
 // Update contact by id
 const updateContact = async (req, res, next) => {
   const { contactId } = req.params;
-  const result = await service.updateContact(contactId, req.body);
+  const { _id } = req.user;
+  const result = await service.updateContact({
+    contactId,
+    _id,
+    ...req.body,
+  });
   if (!result) {
-    return next(createError(404, `Not found contact with id: ${contactId}`));
+    return next(new NotFound(`Not found contact with id: ${contactId}`));
   }
   res.json(result);
 };
@@ -37,9 +45,10 @@ const updateContact = async (req, res, next) => {
 const updateStatusContact = async (req, res, next) => {
   const { contactId } = req.params;
   const { favorite } = req.body;
-  const result = await service.updateStatusContact(contactId, favorite);
+  const { _id } = req.user;
+  const result = await service.updateStatusContact(contactId, _id, favorite);
   if (!result) {
-    return next(createError(404, `Not found contact with id: ${contactId}`));
+    return next(new NotFound(`Not found contact with id: ${contactId}`));
   }
   res.json(result);
 };
@@ -47,9 +56,10 @@ const updateStatusContact = async (req, res, next) => {
 // Delete contact by id
 const deleteContact = async (req, res, next) => {
   const { contactId } = req.params;
-  const result = await service.deleteContact(contactId);
+  const { _id } = req.user;
+  const result = await service.deleteContact(contactId, _id);
   if (!result) {
-    return next(createError(404, `Not found contact with id: ${contactId}`));
+    return next(new NotFound(`Not found contact with id: ${contactId}`));
   }
   res.json({
     message: "contact deleted",
