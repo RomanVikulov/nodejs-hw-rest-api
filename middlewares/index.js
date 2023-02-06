1772b43c5cf11e7ef6427d8e2062c9267b046493
Roman <vikulov.roman.work@gmail.com>
2023-02-06 10:52:53 +0200

    Update index.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const { validation } = require('./validation');
 const { ctrlWrapper } = require('./ctrlWrapper');
 const { validateID } = require('./validateID');
+const { auth } = require('./auth');
 
 module.exports = {
   validation,
   ctrlWrapper,
   validateID,
+  auth,
 };
