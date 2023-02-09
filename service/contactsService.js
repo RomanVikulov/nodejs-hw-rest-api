b33b8089dac395b1e2ed31b865ae34cffbbc3daa
Roman <vikulov.roman.work@gmail.com>
2023-02-09 09:32:36 +0200

    Update contactsService.js
    переробив з фільтром

--- service/contactsService.js
+++ service/contactsService.js
@@ -1,19 +1,35 @@
 const { Contact } = require('../models/contactsModel');
 
 // Get all contacts
-const getAll = async (userId, query) => {
-  const { page = 1, limit = 20, favorite } = query;
+const getAllContacts = async (userId, query) => {
+  const { page = 1, limit = 20, favorite, name, email } = query;
   const skip = (page - 1) * limit;
-  if (favorite)
-    return Contact.find({ owner: userId, favorite: favorite }, '', {
-      skip,
-      limit: +limit,
-    }).populate('owner', '_id username email subscription');
-
-  return Contact.find({ owner: userId }, '', {
+  const pagination = {
     skip,
     limit: +limit,
-  }).populate('owner', '_id username email subscription');
+  };
+
+  const filter = {};
+  if (userId) filter.owner = userId;
+  if (favorite) filter.favorite = favorite;
+  if (name) filter.name = { $regex: name };
+  if (email) filter.email = { $regex: email };
+
+  const ownerInfo = "_id email";
+  return Contact.find(filter, "", pagination)
+    .populate("owner", ownerInfo)
+    .sort({ name: 1 });
+};
+
+const getAll = async (req, res) => {
+  const { _id } = req.user;
+  const result = await getAllContacts(_id, req.query);
+  res.status(200).json({
+    status: "success",
+    data: {
+      result,
+    },
+  });
 };
 
 // Get contacts by id
