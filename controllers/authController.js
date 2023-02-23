541b2d0b34654d32396bac02c8a52dd355e8fbbf
Roman <vikulov.roman.work@gmail.com>
2023-02-23 10:20:07 +0200

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,36 +1,28 @@
-const service = require('../service/authService');
+const service = require("../service/authService");
 
 // Registration
 const signup = async (req, res) => {
-    const user = await service.signup(req.body);
-    const { username, email, subscription } = user;
-    res.status(201).json({ user: { username, email, subscription } });
-  };
+  const user = await service.signup(req.body);
+  res.status(201).json({
+    user: {
+      username: user.username,
+      email: user.email,
+      subscription: user.subscription,
+      avatarURL: user.avatarURL,
+    },
+  });
+};
 
-  // Login
-  const login = async (req, res) => {
+// Login
+const login = async (req, res) => {
   const { token, userWithToken } = await service.login(req.body);
-  const { username, email, subscription } = userWithToken;
-  res.json({ token: token, user: { username, email, subscription } });
-  };
+  res.json({ token: token, user: userWithToken });
+};
 
-  // Logout
+// Logout
 const logout = async (req, res) => {
-    await service.logout(req.user._id);
-    res.status(204).json();
-  };
-  
-  // Get the current user by token
-  const getUser = async (req, res) => {
-    const { username, email, subscription, createdAt, updatedAt } = req.user;
-    res.json({ user: { username, email, subscription, createdAt, updatedAt } });
-  };
-  
-  // Update the current user's subscription
-  const updateUser = async (req, res) => {
-    const user = await service.updateUser(req.user._id, req.body);
-    const { username, email, subscription } = user;
-    res.json({ user: { username, email, subscription } });
-  };
-  
-  module.exports = { signup, login, logout, getUser, updateUser };
+  await service.logout(req.user._id);
+  res.status(204).json();
+};
+
+module.exports = { signup, login, logout };
